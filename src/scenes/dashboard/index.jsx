812ddbe51c40aa8248f203a4162c0e4e040855b9
Trevor Colen <trevorcolen1@gmail.com>
2024-08-27 12:17:56 -0500

    Finishing out most of the sidebar layout as well as the top bar as well getting ready to go into the main aspect of the funcitons of the application.

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../../components/header";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
-}
-
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to your dashboard bud" />
+      </Box>
+    </Box>
+  );
+};
 export default Dashboard;
